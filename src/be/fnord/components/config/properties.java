e1b5d3b6411d8e5c85fa3c1adff88d95f04a5ce7
edm92 <edm92@uowmail.edu.au>
2013-08-02 19:25:22 +1000

    Adding CaNet Generation to TextSeer

---
+++ src/be/fnord/components/config/properties.java
@@ -0,0 +1,64 @@
+package be.fnord.components.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class properties{
+        public static String databaseDriver ="com.mysql.jdbc.Driver";
+        public static String databaseServer ="mysql://localhost/";
+        public static String databaseName ="activiti";
+        public static String databaseUser ="root";
+        public static String databasePass ="";
+        public static boolean debugMode = true;
+        public static String endl ="";
+        public static String sls = "";
+        public static long sessionLength = 0;
+        
+        public static boolean loaded = false;
+
+        public static void loadProperties(){
+                a.e.endl = endl = System.getProperty("line.separator");
+
+                if(loaded) return;
+                loaded = true;
+                
+                try {
+                        properties newP = new properties();
+                    Properties in = newP.getPropertiesFromClasspath("config.ini");
+                    if(in == null) {System.out.println("Null in"); System.exit(-1); };
+                    
+                    databaseDriver = in.getProperty("databaseDriver");
+                    databaseServer = in.getProperty("databaseServer");
+                    databaseName = in.getProperty("databaseName");
+                    databaseUser = in.getProperty("databaseUser");
+                    databasePass = in.getProperty("databasePass");
+                    debugMode = in.getProperty("debugMode").toLowerCase().compareTo("true") == 0? true : false;
+                    sessionLength = Long.parseLong(in.getProperty("sessionLength"));
+                    if(sessionLength == 0) sessionLength = 1209600000;  // If 0 then make it 1 weeks
+                } catch (IOException e) {
+                        System.err.println("Need properties loaded, can not start");
+                        e.printStackTrace();
+                        System.exit(-1);
+                }
+
+        }
+        
+        private Properties getPropertiesFromClasspath(String propFileName)
+            throws IOException
+        {
+        Properties props = new Properties();
+        InputStream inputStream =
+        this.getClass().getClassLoader().getResourceAsStream(propFileName);
+        
+        if (inputStream == null)
+        {
+        throw new FileNotFoundException("property file '" + propFileName
+        + "' not found in the classpath");
+        }
+        
+        props.load(inputStream);
+        return props;
+        }
+}
---
+++ src/be/fnord/components/dataconnection/intConnection.java
@@ -0,0 +1,75 @@
+package be.fnord.components.dataconnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+
+
+public interface intConnection {
+        Connection connect = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        
+        /**
+         * Make a connection to a database using a particular driver
+         * @param driver        e.g. com.mysql.jdbc.Driver
+         * @param serverName e.g. mysql://localhost/
+         * @param database e.g. feedback
+         * @param userName e.g. root
+         * @param password e.g. pass
+         * @return
+         */
+        public boolean makeConnection(String driver, 
+                                        String serverName, 
+                                        String database,
+                                        String userName, 
+                                        String password);
+        
+        /**
+         * Make a connection to a database using default mysql connector
+         * @param database
+         * @param userName
+         * @param password
+         * @return
+         */
+        public boolean makeConnection(
+                        String database,
+                        String userName, 
+                        String password);
+        
+        /**
+         * Execute an SQL query
+         * @param query
+         * @return
+         */
+        public ResultSet executeQuery(String query) ;
+        
+        /** Created a prepared statement. Should be executed using executeQuery.execute  * 
+         * @param query
+         * @return
+         */
+        public PreparedStatement createPreparedStatement(String query);
+        
+        /**
+         * Return result column headers
+         * @param results
+         * @return
+         */
+        public List<String[]> getMetaDataResults(ResultSet results);
+        
+        /** 
+         * Return results from query
+         * @param results
+         * @return
+         */
+        public Map<String, List<Object>> getDataResults(ResultSet results);     
+        /**
+         * Close the current connection
+         * @return
+         */
+        public boolean closeConnection() ;
+}

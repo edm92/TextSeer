fc8e6ba25e6213cb35f3aa3d67461dac6228b38c
edm92 <edm92@uowmail.edu.au>
2014-04-01 16:08:14 +1100

    updated stuff

---
+++ src/be/fnord/util/QUAL/Accumulate.java
@@ -0,0 +1,16 @@
+package be.fnord.util.QUAL;
+
+import orbital.logic.imp.Formula;
+
+import org.apache.log4j.Logger;
+
+public class Accumulate {
+    private static final long serialVersionUID = 1L;
+
+    protected transient static Logger logger = Logger.getLogger("QUALAccumulation");
+
+	public static void main(String[] args) {
+
+	}
+
+}

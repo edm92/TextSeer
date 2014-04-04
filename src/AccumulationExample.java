dc8d5d7204080d07cecf4128ae6455f6a76fa357
edm92 <edm92@uowmail.edu.au>
2014-04-05 10:07:09 +1100

    updated acc functin - now takes Json effects - docs coming now

--- src/AccumulationExample.java
+++ src/AccumulationExample.java
@@ -17,13 +17,25 @@ import java.util.LinkedList;
  * The accumulation example will demonstrate the effect accumulation process over a set of models
  * <p/>
  * <p/>
- * Example Output:
- * Effect scenario resulting from acc: (b) & (c)
- * Checking if (b) & (c) is consistent : true
- * Effect scenario resulting from acc: (a) & (c)
- * Checking if (a) & (c) is consistent : true
- * Effect scenario resulting from acc: ((a & b)) & (~c)
- * Checking if ((a & b)) & (~c) is consistent : true
+ * 
+ * Notes on the accumulation process
+ * 
+ * Example Output (using normal effects):
+ 	Effect scenario resulting from acc: c & a
+		Checking if c & a is consistent : true
+	Effect scenario resulting from acc: c & b
+		Checking if c & b is consistent : true
+	Effect scenario resulting from acc: b & ~c & a
+		Checking if b & ~c & a is consistent : true
+----------------------------------------------------------------
+ * Example Output (using QOS + effects):
+    Effect scenario resulting from acc: ~B & C & A
+		Checking if ~B & C & A is consistent : true
+	QOS Based Effect scenario resulting from acc: QOS:Cost = 40.0; TIME = PT1H30M0.023S; SKILL = HIGH; EFFECT:A; CONSTRAINT: A; GOAL: A; RESOURCE: A;
+	Effect scenario resulting from acc: ~B & ~C & A
+		Checking if ~B & ~C & A is consistent : true
+	QOS Based Effect scenario resulting from acc: QOS:Cost = 40.0; TIME = PT1H20M; SKILL = HIGH; EFFECT:~C; CONSTRAINT: ~C; GOAL: ~C; RESOURCE: ~C;
+
  *
  * @author Evan Morrison edm92@uowmail.edu.au http://www.fnord.be
  *         Apache License, Version 2.0, Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
@@ -46,7 +58,8 @@ public class AccumulationExample {
         // Iterate through each trace.
         if (traces != null) {
             for (Trace t : traces) {
-//				System.out.println("New Model");
+            	
+//				a.e.println("New Model");
 //				for(Vertex v: t.getNodes()){
 //					a.e.println("Vertex effect " + v.getWFF());
 //				}
@@ -55,14 +68,15 @@ public class AccumulationExample {
                 // Display Results
                 for (WFF e : _ee) {
                     if (e.isEmpty()) continue;
-//                    WFF newE = new WFF(e.getClosure());	// Clean up the result a little bit 
-                    a.e.println("Effect scenario resulting from acc: " + e);                    
-                    a.e.println("\tChecking if " + e + " is consistent : " + e.issat());
+                    WFF newE = new WFF(e.getClosure());	// Clean up the result a little bit 
+                    a.e.println("Effect scenario resulting from acc: " + newE);                    
+                    a.e.println("\tChecking if " + newE + " is consistent : " + newE.issat());
 
                 }
              // Display Results
                 for (JSONEFFECT e : _ee2) {
                     if (e.isEmpty()) continue;
+                    a.e.println(e.Name + " " + e.Type);
                     a.e.println("QOS Based Effect scenario resulting from acc: " + e);
                 }
             }
@@ -76,7 +90,9 @@ public class AccumulationExample {
         LinkedList<Trace> traces = new LinkedList<Trace>();
         // For details of below refer to Decision free graph conversion and model loading
         GraphLoader gLoader = new GraphLoader();
-        Graph<Vertex, Edge> g1 = gLoader.loadModel("models/Benefits Administration - Ongoing.bpmn20.xml", a.e.DONT_SAVE_MESSAGES_AND_PARTICIPANTS);
+//        Graph<Vertex, Edge> g1 = gLoader.loadModel("models/Benefits Administration - Ongoing.bpmn20.xml", a.e.DONT_SAVE_MESSAGES_AND_PARTICIPANTS);
+        Graph<Vertex, Edge> g1 = gLoader.loadModel("models/Model1.bpmn20.xml", a.e.DONT_SAVE_MESSAGES_AND_PARTICIPANTS);
+        
         LinkedList<Graph<Vertex, Edge>> _decisionless = GraphTransformer.makeDecisionFree(g1);
         
         LinkedList<Graph<Vertex, Edge>> decisionless = GraphTransformer.removeDupesFromDecisionFreeGraphs(_decisionless);

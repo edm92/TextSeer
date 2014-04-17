9b9a7e3528375a725fd9a8c911dfb70fecf57a58
edm92 <edm92@uow.edu.au>
2014-04-17 23:39:38 +1000

    Adding a sentiment checker ... because!

---
+++ src/main/java/be/fnord/util/functions/language/Sentiment.java
@@ -0,0 +1,59 @@
+package be.fnord.util.functions.language;
+import java.util.Properties;
+
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+
+
+public class Sentiment {
+
+
+	StanfordCoreNLP pipeline; 
+
+
+	public static void main(String [] args){
+		Sentiment s = new Sentiment();
+		String ss = "Turn the light off";
+		a.e.println("Sentiment for " + ss + " is " + s.calculateSentiment(ss));
+	}
+
+	public Sentiment(){
+	    Properties props = new Properties();
+	    props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+
+	    pipeline = new StanfordCoreNLP(props);
+
+
+	}
+
+	public float calculateSentiment (String text) {
+
+
+	        float mainSentiment = 0;
+
+	        int longest = 0;
+	        Annotation annotation = pipeline.process(text);
+	        for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+	            Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
+	            int sentiment = RNNCoreAnnotations.getPredictedClass(tree) - 2;
+	            String partText = sentence.toString();
+	            if (partText.length() > longest) {
+	                mainSentiment = sentiment;
+	                longest = partText.length();
+	            }
+
+	        }
+
+	       return mainSentiment;
+
+
+
+	}
+
+}
